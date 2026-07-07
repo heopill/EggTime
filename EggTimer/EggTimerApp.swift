f328dcea170847d281ee15fc545051f3f0ff1da4
heopill <heopill@naver.com>
2026-07-07 23:57:44 +0900

    Initial commit: EggTimer project setup

---
+++ EggTimer/EggTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggTimerApp.swift
+//  EggTimer
+//
+//  Created by 허성필 on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct EggTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
