e4d1c2abf4f3eae5f8e553a570c6689b4cdc49bf
heopill <heopill@naver.com>
2026-07-18 22:58:05 +0900

    [Feat] #20 - iOS 시스템 배너 알림 기능 추가

--- EggTimer/EggTimerApp.swift
+++ EggTimer/EggTimerApp.swift
@@ -7,9 +7,31 @@
 
 import SwiftUI
 import ComposableArchitecture
+import UserNotifications
+
+// 포그라운드에서도 시스템 배너를 띄우기 위한 알림 델리게이트
+final class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+
+        return true
+    }
+
+    // 앱이 켜져 있을 때도 배너 + 소리로 알림을 표시한다
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification
+    ) async -> UNNotificationPresentationOptions {
+        return [.banner, .sound, .list]
+    }
+}
 
 @main
 struct EggTimerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     @State private var showSplash = true
 
     var body: some Scene {
