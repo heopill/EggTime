115152005bf42c4f63e4af72d8f1f6cf414a4d5f
heopill <heopill@naver.com>
2026-09-08 20:46:06 +0900

    [Chore] #51 - 기본 더미 위젯 제거

--- EggTimerWidget/EggTimerWidgetBundle.swift
+++ EggTimerWidget/EggTimerWidgetBundle.swift
@@ -11,11 +11,6 @@ import SwiftUI
 @main
 struct EggTimerWidgetBundle: WidgetBundle {
     var body: some Widget {
-        EggTimerWidget()
-        // Control 위젯은 iOS 18.0 이상에서만 지원된다
-        if #available(iOS 18.0, *) {
-            EggTimerWidgetControl()
-        }
         EggTimerWidgetLiveActivity()
     }
 }
